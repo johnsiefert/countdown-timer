7f822f6acdb2f81cf8e8fb055dbd5603b87ad2d6
John <johnsief12@gmail.com>
2024-02-02 17:27:19 -0500

    dynamically added giveway day

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const items = document.querySelectorAll('.deadline-format h4');
+const deadline = document.querySelector('.deadline');
+
+let futureDate = new Date(2024, 4, 24, 11,30,0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()]
+console.log(weekday);
+
+giveaway.textContent = `giveaway ends on  ${weekday} ${date}, ${month} ${year} ${hours}:${minutes}am`;
