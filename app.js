8e868fe62fc19059fd0127ab0471896f9c269e2e
John <johnsief12@gmail.com>
2024-02-03 12:43:37 -0500

    created functional countdown

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
 
 const giveaway = document.querySelector('.giveaway');
 const items = document.querySelectorAll('.deadline-format h4');
 const deadline = document.querySelector('.deadline');
 
-let futureDate = new Date(2024, 4, 24, 11,30,0);
+let futureDate = new Date(2024, 4, 24, 11, 30, 0);
 // console.log(futureDate);
 
 const year = futureDate.getFullYear();
@@ -39,42 +39,52 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on  ${weekday} ${date}, ${month} ${year} ${hours}:${minutes}am`;
+giveaway.textContent = `giveaway ends on  ${weekday} ${date}, ${month} ${year} ${hours}:${minutes}`;
 
 // items.forEach(function (item) {
 //     console.log(item);
 //   });
 
-  //future time in ms
-  const futureTime = futureDate.getTime();
+//future time in ms
+const futureTime = futureDate.getTime();
 
-  function getRemainingTime(){
-    //current time in ms
-    const today = new Date().getTime();
-    const time = futureTime - today;
-    //1s = 1000ms
-    // 1min = 60s
-    // 1hr = 60min
-    // 1d = 24hr
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000;
-    const oneMinute = 60 * 1000;
+function getRemainingTime() {
+  //current time in ms
+  const today = new Date().getTime();
+  const time = futureTime - today;
+  //1s = 1000ms
+  // 1min = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
 
+  let days = time / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((time % oneDay) / oneHour); // how many days then how many hours
+  let mins = Math.floor((time % oneHour) / oneMinute);
+  let seconds = Math.floor((time % oneMinute) / 1000);
 
+  //set values array
+  const values = [days, hours, mins, seconds];
+  function format(item){
+    if(item < 10){
+      return item = `0${item}`
+    }
+    return item
+  }
 
-    let days = time / oneDay;
+  items.forEach(function(item, index){
+    item.innerHTML = format(values[index]);
+  });
+if(time < 0){
+  clearInterval(countdown);
+  deadline.innerHTML = `<h4 class ="expired"> sorry, this giveaway has expired.</h4>`
+}
+}
 
-    // if(futureTime > 0){
-    //   items.forEach(function(item){
-    //     console.log(item);
-    //   });
-    // }
-      // setTimeout(getRemainingTime,1000)
-  };
-  getRemainingTime()
+//countdown
+let countdown = setInterval(getRemainingTime, 1000);
 
-
-
-
-
-
+getRemainingTime();
