8d454e7980566ee6346458299eb7ae678b981495
John <johnsief12@gmail.com>
2024-02-03 12:11:25 -0500

    created variables for time

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const items = document.querySelectorAll('.deadline-format h4');
 const deadline = document.querySelector('.deadline');
 
 let futureDate = new Date(2024, 4, 24, 11,30,0);
-console.log(futureDate);
+// console.log(futureDate);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -37,7 +37,44 @@ let month = futureDate.getMonth();
 month = months[month];
 const date = futureDate.getDate();
 
-const weekday = weekdays[futureDate.getDay()]
-console.log(weekday);
+const weekday = weekdays[futureDate.getDay()];
 
 giveaway.textContent = `giveaway ends on  ${weekday} ${date}, ${month} ${year} ${hours}:${minutes}am`;
+
+// items.forEach(function (item) {
+//     console.log(item);
+//   });
+
+  //future time in ms
+  const futureTime = futureDate.getTime();
+
+  function getRemainingTime(){
+    //current time in ms
+    const today = new Date().getTime();
+    const time = futureTime - today;
+    //1s = 1000ms
+    // 1min = 60s
+    // 1hr = 60min
+    // 1d = 24hr
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+
+
+    let days = time / oneDay;
+
+    // if(futureTime > 0){
+    //   items.forEach(function(item){
+    //     console.log(item);
+    //   });
+    // }
+      // setTimeout(getRemainingTime,1000)
+  };
+  getRemainingTime()
+
+
+
+
+
+
